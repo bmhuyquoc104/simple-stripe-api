697d18389d3c7762580e4bb19fccda6f7d9a1b1e
Huy Nè <bmhuyquoc104@gmail.com>
2022-06-20 21:51:17 +0700

    8. Finish create cart for checkout

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,12 +7,14 @@ import {
   CardMedia,
   CardActions,
   IconButton,
+  Button,
   CardContent,
 } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 function Home() {
   const [products, setProducts] = useState<any>([]);
+  const [cart, setCart] = useState<any>([]);
 
   useEffect(() => {
     async function getData() {
@@ -27,7 +29,7 @@ function Home() {
     getData();
   }, []);
 
-  const changeQuantity = (value: number, index: number, e: any) => {
+  const changeQuantity = async (value: number, index: number, e: any) => {
     setProducts(
       products.map((product: any) => {
         if (product.id === index) {
@@ -41,42 +43,71 @@ function Home() {
     );
   };
 
+  const addCart = () => {
+    console.log("hello");
+    const product = products.filter((element: any) => element.quantity > 0);
+    if (cart.includes(product)) return
+    setCart(product);
+  };
+
+  console.log(cart);
+
   return (
-    <Grid container spacing={4}>
-      {products?.map((product: any) => (
-        <Grid key={product.id} item>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              sx={{ objectFit: "contain" }}
-              height="200"
-              alt={`product-${product.id}-logo`}
-              image={product.image}
-            />
-            <CardContent>
-              <Stack direction="column" spacing={2}>
-                <Typography>{product.title}</Typography>
-                <Typography variant="h6" sx={{ color: "red" }}>
-                  {product.price}
-                </Typography>
-                <Typography sx={{ maxHeight: 120, overflowY: "scroll" }}>
-                  {product.description}
-                </Typography>
-              </Stack>
-            </CardContent>
-            <CardActions>
-              <IconButton onClick={(e) => changeQuantity(1, product.id, e)}>
-                <AddCircleIcon /> 
-              </IconButton>
-              <Typography>{product?.quantity}</Typography>
-              <IconButton onClick={(e) => changeQuantity(-1, product.id, e)}>
-                <RemoveCircleIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <Stack direction="row" spacing={2}>
+      <Grid
+        justifyContent="space-evenly"
+        sx={{ flexGrow: 1 }}
+        container
+        spacing={4}
+      >
+        {products?.map((product: any) => (
+          <Grid key={product.id} item>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                sx={{ objectFit: "contain" }}
+                height="200"
+                alt={`product-${product.id}-logo`}
+                image={product.image}
+              />
+              <CardContent>
+                <Stack direction="column" spacing={2}>
+                  <Typography>{product.title}</Typography>
+                  <Typography variant="h6" sx={{ color: "red" }}>
+                    {product.price}
+                  </Typography>
+                  <Typography sx={{ maxHeight: 120, overflowY: "scroll" }}>
+                    {product.description}
+                  </Typography>
+                </Stack>
+              </CardContent>
+              <CardActions>
+                <IconButton
+                  onClick={(e) => {
+                    changeQuantity(1, product.id, e);
+                  }}
+                >
+                  <AddCircleIcon />
+                </IconButton>
+                <Typography>{product?.quantity}</Typography>
+                <IconButton onClick={(e) => changeQuantity(-1, product.id, e)}>
+                  <RemoveCircleIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      <Button
+        sx={{ width: "300px", alignSelf: "start" }}
+        variant="contained"
+        color="error"
+        size="large"
+        onClick = {addCart}
+      >
+        Check Out
+      </Button>
+    </Stack>
   );
 }
 
